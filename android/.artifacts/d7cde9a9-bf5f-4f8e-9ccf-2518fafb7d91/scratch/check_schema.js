34a702ec077a23ba95ee5b050bc4cd9806275025
Brian Ellsworth <physicsstudent137@gmail.com>
2026-07-28 17:15:22 -0400

    Clean up scratch scripts

--- android/.artifacts/d7cde9a9-bf5f-4f8e-9ccf-2518fafb7d91/scratch/check_schema.js
+++
@@ -1,5 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('sacred_ring_data/catalog/hooktheory_catalog.db');
-const schema = db.prepare("SELECT sql FROM sqlite_master WHERE type='table' AND name='songs'").get();
-console.log(schema.sql);
-db.close();
--- android/.artifacts/d7cde9a9-bf5f-4f8e-9ccf-2518fafb7d91/scratch/get_file_size.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const stats = fs.statSync('sacred_ring_data/catalog/harvested_songs.db');
-console.log(`Size: ${(stats.size / 1024 / 1024).toFixed(2)} MB`);
